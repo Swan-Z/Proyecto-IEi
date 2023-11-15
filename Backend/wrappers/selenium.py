9e9e38b4c037e49eafc79a6cfd4ea2b8e6aaf390
Danko111 <bruno93op@gmail.com>
2023-11-15 14:19:35 +0100

    siuuuu

--- Backend/wrappers/selenium.py
+++ Backend/wrappers/selenium.py
@@ -1,12 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 class selenium:
 
     def VerificarTitulo():
         driver_path = 'path/to/chromedriver';
-        driver = webdriver.Chrome(executable.path=driver_path);
+        driver = webdriver.Chrome(executable_path=driver_path);
         driver.get("https://www.coordenadas-gps.com/");
-        element = driver.findElement(By.id("address"));
+        element = driver.find_element("id","address")
         element.sendKeys("Universidad Politécnica de Valencia");
         element.submit();
     
