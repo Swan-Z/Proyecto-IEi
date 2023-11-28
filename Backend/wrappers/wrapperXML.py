34ca42b425e0b3793f71a103fd326b35546d61e8
Yu Lin <YLIN2@upv.edu.es>
2023-11-28 12:01:40 +0100

    comentado xml

--- Backend/wrappers/wrapperXML.py
+++ Backend/wrappers/wrapperXML.py
@@ -4,11 +4,11 @@ import os
 
 #Los comentados es para probar directamente en este fichero
 #python wrappers/fichero.py
-directorio_actual = os.getcwd()
-rutaXML = 'ficheroFuenteDatos/CAT.xml'
-rutaJSON = 'jsonResultFromWrapper/CAT.json'
-rutaComXML = os.path.abspath(os.path.join(directorio_actual, rutaXML))
-rutaComJSON = os.path.abspath(os.path.join(directorio_actual, rutaJSON))
+# directorio_actual = os.getcwd()
+# rutaXML = 'ficheroFuenteDatos/CAT.xml'
+# rutaJSON = 'jsonResultFromWrapper/CAT.json'
+# rutaComXML = os.path.abspath(os.path.join(directorio_actual, rutaXML))
+# rutaComJSON = os.path.abspath(os.path.join(directorio_actual, rutaJSON))
 
 def XML_to_JSON(pathXML, pathJSON):
     with open(pathXML, 'rb') as file:
@@ -19,4 +19,4 @@ def XML_to_JSON(pathXML, pathJSON):
 
     df.to_json(pathJSON, orient='records', indent=2)
     
-XML_to_JSON(rutaComXML, rutaComJSON)
+# XML_to_JSON(rutaComXML, rutaComJSON)
