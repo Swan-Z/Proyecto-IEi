f68cac6d5db8a1f5fddb8078f39f8091059e897c
Yu Lin <YLIN2@upv.edu.es>
2023-12-01 11:29:02 +0100

    avances en exJosn

--- Backend/extractors/extractorJSON.py
+++ Backend/extractors/extractorJSON.py
@@ -4,75 +4,97 @@ import re
 
 def json_a_json():
     datos = []
+    inserta = True
     directorio_actual = os.getcwd()
     rutaJSON = 'jsonResultFromWrapper/MUR.json'
     rutaNuevo = 'jsonResultFromWrapper/MUR_Nuevo.json'
     rutaComJSON = os.path.abspath(os.path.join(directorio_actual, rutaJSON))
     rutaComNuevo = os.path.abspath(os.path.join(directorio_actual, rutaNuevo))
+    
 
     # Abrir el archivo JSON y leer los datos con la codificación 'utf-8'
     with open(rutaComJSON, 'r', encoding='utf-8') as archivo:
         lector_json = json.load(archivo)
         for fila in lector_json:
-
+            inserta = True
             # Renombrar claves si existen
             if 'dencen' in fila:
-                fila['C_E.nombre'] = fila.pop('dencen')
+                fila['nombre'] = fila.pop('dencen')
             else:
+                inserta = False
                 print('No existe la clave dencen')
                 print(fila)
 
             if 'tipo' in fila:
-                if 'concertado' in fila['presentacionCorta']:
-                    fila['C_E.tipo'] = 'concertado'
-                elif 'Privado' in fila['tipo']:
-                    fila['C_E.tipo'] = 'privado'
-                elif 'Público' in fila['tipo']:
-                    fila['C_E.tipo'] = 'publico'
+                if 'concertad' in fila['presentacionCorta'].lower() or fila['denLarga'].lower() or fila['tipo'].lower():
+                    fila['tipo'] = 'Concertado'
                 else:
-                    fila['C_E.tipo'] = 'Otros'
-            else:
-                print('No detecta el REGIMEN')
-                print(fila) 
+                    if 'privado' in fila['tipo'].lower():
+                        fila['tipo'] = 'Privado'
+                    if 'público' in fila['tipo'].lower():
+                        fila['tipo'] = 'Público'
+                    else:
+                        print(fila['tipo'])
+                        fila['tipo'] = 'Otros'
+
             if 'domcen' in fila:
-                fila['C_E.direccion'] = fila.pop('domcen')
+                fila['direccion'] = fila.pop('domcen')
             else:
-                print('No existe la clave domcen')
+                inserta = False
+                print('No existe la dirección')
                 print(fila)
+                
             if 'cpcen' in fila:
-                cp = fila['cpcen']
-                fila['C_E.codigo_postal'] = str(cp).zfill(5) 
-                fila['LOC.codigo'] = str(cp).zfill(5)
-                fila['PRO.codigo'] = re.search(r'\d{2}', str(cp).zfill(5)).group()
+                fila['codigo_postal'] = fila.pop('cpcen')
+                #cp = fila['cpcen']
+                #fila['C_E.codigo_postal'] = str(cp).zfill(5) 
+                #fila['LOC.codigo'] = str(cp).zfill(5)
+                #fila['PRO.codigo'] = re.search(r'\d{2}', str(cp).zfill(5)).group()
             else:
+                fila['codigo_postal'] = None
                 print('No existe la clave cpcen')
                 print(fila)
+                
             if 'telcen' in fila:
-                fila['C_E.telefono'] = fila.pop('telcen')
+                if len(fila['telcen']) == 9:
+                    fila['telefono'] = fila.pop('telcen')
+                else:
+                    print('Número major o minor que 9 dígits')
+                    fila['telefono'] = None
             else:
+                if 'telcen2' in fila:
+                    if len(fila['telcen2']) == 9:
+                        fila['telefono'] = fila.pop('telcen2')
+                    else:
+                        print('Número major o minor que 9 dígits')
+                        fila['telefono'] = None
                 # print('No existe la clave telcen')
-                # print(fila)
-                fila['C_E.telefono'] = None
+                else:
+                    print(fila)
+                    fila['telefono'] = None
+                
             if 'presentacionCorta' in fila:
-                fila['C_E.descripcion'] = fila.pop('presentacionCorta')
+                fila['descripcion'] = fila.pop('presentacionCorta')
             else:
+                fila['descripcion'] = None
                 print('No existe la clave presentacionCorta')
                 print(fila)
 
             # Mover datos de georeferencia si existen
             if 'geo-referencia' in fila:
                 if 'lon' in fila['geo-referencia']:
-                    fila['C_E.longitud'] = fila['geo-referencia'].pop('lon')
+                    fila['longitud'] = fila['geo-referencia'].pop('lon')
                 else:
+                    fila['longitud'] = None
                     print('No existe la clave lon')
-                    print(fila)
                 if 'lat' in fila['geo-referencia']:
-                    fila['C_E.latitud'] = fila['geo-referencia'].pop('lat')
+                    fila['latitud'] = fila['geo-referencia'].pop('lat')
                 else:
+                    fila['latitud'] = None
                     print('No existe la clave lat')
-                    print(fila)
+                    
             if 'loccen' in fila:
-                fila['LOC.nombre'] = fila.pop('loccen')
+                fila['codigo_postal'] = fila.pop('loccen')
             else:
                 print('No existe la clave loccen')
                 print(fila)
@@ -83,20 +105,18 @@ def json_a_json():
                 print(fila)
 
             nuevo_dato = {
-                'C_E.nombre': fila['C_E.nombre'],
-                'C_E.tipo': fila['C_E.tipo'],
-                'C_E.direccion': fila['C_E.direccion'],
-                'C_E.codigo_postal': fila['C_E.codigo_postal'],
-                'C_E.telefono': fila['C_E.telefono'],
-                'C_E.descripcion': fila['C_E.descripcion'],
-                'LOC.nombre': fila['LOC.nombre'],
-                'LOC.codigo': fila['LOC.codigo'],
-                'PRO.nombre': fila['PRO.nombre'],
-                'PRO.codigo': fila['PRO.codigo']
+                'nombre': fila['nombre'],
+                'tipo': fila['tipo'],
+                'direccion': fila['direccion'],
+                'codigo_postal': fila['codigo_postal'],
+                'telefono': fila['telefono'],
+                'descripcion': fila['descripcion'],
+                'longitud': fila['longitud'],
+                'latitud': fila['latitud'],
             }
-            datos.append(nuevo_dato)
+            # datos.append(nuevo_dato)
 
-        with open(rutaComNuevo, 'w', encoding='utf-8') as archivoNuevo:
-            json.dump(datos, archivoNuevo, indent=2, ensure_ascii=False)
+        # with open(rutaComNuevo, 'w', encoding='utf-8') as archivoNuevo:
+        #     json.dump(datos, archivoNuevo, indent=2, ensure_ascii=False)
 
 json_a_json()
