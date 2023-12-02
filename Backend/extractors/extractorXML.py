de4a494eac90454aa6160f7a73eba440853d2f32
Jose Francisco Garcia Sanchez <JFGARSAN@upv.edu.es>
2023-12-02 20:40:23 +0100

    haciendo extractor xml, funciona mal lo pone todo a none

--- Backend/extractors/extractorXML.py
+++ Backend/extractors/extractorXML.py
@@ -1,9 +1,14 @@
 import json
 import os
 import re  
+import sys 
+
+ruta_backend = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(ruta_backend)
+
+from repositorio import *
 
 def xml_a_json():
-    datos = []
     directorio_actual = os.getcwd()
     rutaJSON = 'jsonResultFromWrapper/CAT.json'
     rutaNuevo = 'jsonResultFromWrapper/CAT_Nuevo.json'
@@ -16,23 +21,122 @@ def xml_a_json():
 
         for fila in lector_json:
 
-            
+             # Renombrar claves si existen
+            if 'denominaci_completa' in fila:
+                fila['nombre'] = fila.pop('denominaci_completa')
+            else:  
+                fila['nombre'] = None
+                print('No existe la clave denominaci_completa, por lo tanto esta fila no será insertada')
+                print(fila)
 
-            nuevo_dato = {
-                'C_E.nombre': fila['C_E.nombre'],
-                'C_E.tipo': fila['C_E.tipo'],
-                'C_E.direccion': fila['C_E.direccion'],
-                'C_E.codigo_postal': fila['C_E.codigo_postal'],
-                'C_E.telefono': fila['C_E.telefono'],
-                'C_E.descripcion': fila['C_E.descripcion'],
-                'LOC.nombre': fila['LOC.nombre'],
-                'LOC.codigo': fila['LOC.codigo'],
-                'PRO.nombre': fila['PRO.nombre'],
-                'PRO.codigo': fila['PRO.codigo']
+            if 'nom_naturalesa' in fila:
+                if 'concertat' in fila['nom_naturalesa'].lower():
+                    fila['tipo'] = 'Concertado'
+                else:
+                    if 'privat' in fila['nom_naturalesa'].lower():
+                        fila['tipo'] = 'Privado'
+                    elif 'públic' in fila['nom_naturalesa'].lower():
+                        fila['tipo'] = 'Público'
+                    else:
+                        print('No se ha podido determinar el tipo de centro')
+                        print(fila)
+                        fila['tipo'] = 'Otros'
+            else:
+                fila['tipo'] = 'Otros'
+                print('No existe la clave tipo')
+                print(fila)
+
+
+            if 'adre_a' in fila:
+                fila['direccion'] = fila.pop('adre_a')
+            else:
+                fila['direccion'] = None
+                print('No existe la dirección, por lo tanto esta fila no será insertada')
+                print(fila)
+                
+            if 'codi_postal' in fila:
+                fila['codigo_postal'] = fila.pop('codi_postal')
+                fila['codigo_provincia'] = fila['codigo_postal'][:2]
+            else:
+                fila['codigo_provincia'] = None
+                fila['codigo_postal'] = None
+                fila['nombre_provincia'] = None
+                print('No existe la clave codi_postal')
+                print(fila)
+                
+            
+                
+            if 'estudis' in fila:
+                fila['descripcion'] = fila.pop('estudis')
+            else:
+                fila['descripcion'] = None
+                print('No existe la clave estudis')
+                print(fila)
+
+            # Mover datos de georeferencia si existen
+
+            if 'coordenades_geo_x' in fila:
+                    fila['longitud'] = fila.pop('coordenades_geo_x')
+            else:
+                    fila['longitud'] = None
+                    print('No existe la clave longitud')
+                    print(fila)
+            if 'coordenades_geo_y' in fila:
+                    fila['latitud'] = fila.pop('coordenades_geo_y')
+            else:
+                    fila['latitud'] = None
+                    print('No existe la clave latitud')
+                    print(fila)
+            #-----------------------------------------------------------------
+            if 'nom_municipi' in fila:
+                fila['localidad'] = fila.pop('nom_municipi')
+            else:
+                print('No existe la clave nom_municipi')
+                print(fila)
+                fila['localidad'] = None
+            if fila['codigo_provincia'] != None:
+                if fila['codigo_provincia'] == '08':
+                    fila['nombre_provincia'] = 'Barcelona'
+                elif fila['codigo_provincia'] == '17':
+                    fila['nombre_provincia'] = 'Girona'
+                elif fila['codigo_provincia'] == '25':
+                    fila['nombre_provincia'] = 'Lleida'
+                elif fila['codigo_provincia'] == '43':
+                    fila['nombre_provincia'] = 'Tarragona'
+                else:
+                    print('No se ha podido determinar la provincia')
+                    print(fila)
+                    fila['nombre_provincia'] = None
+
+            #-----------------------------------------------------------------
+
+            datoCentro = {
+                'nombre': fila['nombre'],
+                'tipo': fila['tipo'],
+                'direccion': fila['direccion'],
+                'codigo_postal': fila['codigo_postal'],
+                'telefono': None,
+                'descripcion': fila['descripcion'],
+                'longitud': fila['longitud'],
+                'latitud': fila['latitud'],
             }
-            datos.append(nuevo_dato)
-        
-        with open(rutaComNuevo, 'w', encoding='utf-8') as archivoNuevo:
-            json.dump(datos, archivoNuevo, indent=2, ensure_ascii=False)
+
+            datoProvincia = {
+                'codigo': fila['codigo_provincia'],
+                'nombre': fila['nombre_provincia']
+            }
+                
+
+            datoLocalidad = {
+                'codigo': fila['codigo_postal'],
+                'nombre': fila['localidad'],
+                'en_provincia': fila['nombre_provincia']
+            } 
+
+            if datoCentro['nombre'] != None and datoCentro['direccion'] != None:
+                Repositorio.insertData('Provincia', datoProvincia)
+                Repositorio.insertData('Localidad', datoLocalidad)
+                Repositorio.insertData('Centro_Educativo', datoCentro)
+
 
 xml_a_json()
